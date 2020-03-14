c0aa2197049bdadcd359a2fdd90e05ccda3e0b23
Ozan Yıldız <developer@yildizozan.com>
2020-03-15 02:41:20 +0300

    AMQP and gRPC

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"context"
+	pb "conveyer-service-collector/protos/measurement"
 	"fmt"
 	"github.com/streadway/amqp"
+	"google.golang.org/grpc"
 	"log"
-	"math/rand"
-	"os"
-	"strings"
-	"time"
+	"net"
 )
 
 // AMQP Channel
 var ch *amqp.Channel
+
+type service struct {
+	pb.UnimplementedMeasurementServiceServer
+}
+
+func (s *service) NewMeasurement(ctx context.Context, measurement *pb.Measurement) (*pb.OK, error) {
+	fmt.Println(measurement)
+
+	err := ch.Publish(
+		"logs", // exchange
+		"",     // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(measurement.String()),
+		})
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to publish a message", err)
+	}
+
+	return &pb.OK{
+		Success: true,
+	}, nil
+}
 
 func main() {
 	conn, err := amqp.Dial("amqp://orebron.com:5672/")
@@ -26,6 +51,11 @@ func main() {
 	}
 	defer ch.Close()
 
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
 	err = ch.ExchangeDeclare(
 		"logs",   // name
 		"fanout", // type
@@ -39,32 +69,10 @@ func main() {
 		log.Fatalf("%s: %s", "Failed to declare an exchange", err)
 	}
 
-	rand.Seed(time.Now().UTC().UnixNano())
-	for x := range time.Tick(3 * time.Second) {
-		fmt.Printf("%s\n", x)
-
-		body := bodyFrom(os.Args)
-		err = ch.Publish(
-			"logs", // exchange
-			"",     // routing key
-			false,  // mandatory
-			false,  // immediate
-			amqp.Publishing{
-				ContentType: "text/plain",
-				Body:        []byte(body),
-			})
-		if err != nil {
-			log.Fatalf("%s: %s", "Failed to publish a message", err)
-		}
+	s := grpc.NewServer()
+	pb.RegisterMeasurementServiceServer(s, &service{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
-}
 
-func bodyFrom(args []string) string {
-	var s string
-	if (len(args) < 2) || os.Args[1] == "" {
-		s = fmt.Sprintf("Humidity: %f", rand.Float64())
-	} else {
-		s = strings.Join(args[1:], " ")
-	}
-	return s
 }
